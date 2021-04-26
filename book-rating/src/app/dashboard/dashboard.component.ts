69e4c4e37550c9be3aa6670c78ffe18f7e33d402
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-04-26 16:37:16 +0200

    [object Object],[object Object],[object Object]

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-dashboard',
@@ -7,12 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  books: string[];
+  books: Book[];
 
   constructor() { }
 
   ngOnInit(): void {
-    this.books = ['Angular', 'AngularJS', 'Ext.JS'];
+    this.books = [{
+      isbn: '000',
+      title: 'Angular',
+      description: 'tolles Buch',
+      rating: 5
+    }, {
+      isbn: '111',
+      title: 'AngularJS',
+      description: 'alter Buch',
+      rating: 3
+    }, {
+      isbn: '222',
+      title: 'Ext.JS',
+      description: 'tolles Buch, aber Lizenz zu teuer!',
+      rating: 1
+    }];
   }
 
 }
