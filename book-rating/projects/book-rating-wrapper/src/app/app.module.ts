4ff368ad8e7e19750add6135292b0ff782ab5f05
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-04-27 16:02:31 +0200

    my first web component =)

--- book-rating/projects/book-rating-wrapper/src/app/app.module.ts
+++ book-rating/projects/book-rating-wrapper/src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
+import { DoBootstrap, Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { BookComponent } from 'src/app/books/book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    // AppComponent
+    BookComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    // AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap {
+
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap(): void {
+    const webComponent = createCustomElement(BookComponent, { injector: this.injector });
+    customElements.define('book-component', webComponent);
+  }
+ }
