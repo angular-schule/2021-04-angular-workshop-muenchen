bcf4af512cd9aebc8892a7951d17c1cd88873241
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-04-29 14:17:59 +0200

    hands on: forms

--- book-rating/src/app/books/create-book/create-book.component.ts
+++ book-rating/src/app/books/create-book/create-book.component.ts
@@ -9,14 +9,30 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class CreateBookComponent {
 
   bookForm = new FormGroup({
-    isbn: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    isbn: new FormControl('xxx', [Validators.required, Validators.minLength(3)]),
     title: new FormControl('', Validators.required),
     description: new FormControl()
   });
 
-  isValid(name: string) {
+  isValid(name: string): boolean {
     const control = this.bookForm.get(name);
     return control.touched && control.invalid
+  }
+
+  submitForm(): void {
+
+    const newBook = {
+      ...this.bookForm.value,
+      rating: 1
+    }
+
+    // Hands On
+    // 1. Emitiere ein Event mit dem Namen `create` (Nutzlast `newBook`)
+    // 2. Subscribe dich im Dashboard auf das Event
+    // 3. Füge das neue Buch dem Buch-Array hinzu
+
+
+    this.bookForm.reset();
   }
 
 }
