87bd984b6727c16b5abcbee43cc93e6ac05e80f5
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-04-30 16:09:16 +0200

    rxjs: game-score

--- rxjs-playground/src/app/exercises/game-score/game-score.component.ts
+++ rxjs-playground/src/app/exercises/game-score/game-score.component.ts
@@ -23,9 +23,13 @@ export class GameScoreComponent implements OnInit {
 
     /******************************/
 
-    this.score$.subscribe();
+    this.score$.pipe(
+      scan((acc, item) => acc + item, 0)
+    ).subscribe(score => this.currentScore = score);
 
-    this.score$.subscribe();
+    this.score$.pipe(
+      reduce((acc, item) => acc + item, 0)
+    ).subscribe(score => this.finalScore = score);
 
 
     /******************************/
