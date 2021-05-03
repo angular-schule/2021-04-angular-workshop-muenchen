128a0f8e37786362dc0ad477eef57fed3f75a90d
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-03 16:57:43 +0200

    ng g feature books/store/book --module books/books --api --defaults

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 // import { DashboardComponent } from './dashboard/dashboard.component';
 // import { BookComponent } from './books/book/book.component';
 
@@ -17,7 +21,10 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     // BooksModule,
-    HttpClientModule
+    HttpClientModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
