c3783f8b21948b7d0db9467f0a95391c5a526c85
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-03 15:39:18 +0200

    rxjs: share on details

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -2,7 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
-import { catchError, map, mergeMap, retry, switchMap } from 'rxjs/operators';
+import { catchError, map, mergeMap, retry, share, switchMap } from 'rxjs/operators';
 import { BookStoreService } from '../shared/book-store.service';
 
 
@@ -19,9 +19,11 @@ export class BookDetailsComponent {
       retry(3),
       catchError((err: HttpErrorResponse) => of({
         title: 'FEHLER',
-        description: err.message
+        description: err.message,
+        rating: 0
       }))
     )),
+    share()
   );
 
   constructor(private router: ActivatedRoute, private bs: BookStoreService) {
