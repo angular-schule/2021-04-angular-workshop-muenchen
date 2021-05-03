b48c264b60520265abc85624f98b1c70adfadcbc
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-03 14:10:25 +0200

    angular router: don't use snapshot

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'br-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent {
 
-  constructor() { }
+  // $result =
 
-  ngOnInit(): void {
+  isbn: string;
+
+  constructor(private router: ActivatedRoute) {
+    debugger;
+    this.isbn = router.snapshot.paramMap.get('isbn');
   }
 
 }
