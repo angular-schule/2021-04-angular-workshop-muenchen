1b73a5055611c772733551697d07a561c2bfff98
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-03 15:55:24 +0200

    rxjs: poor mans loading indicator 2

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -2,7 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
-import { catchError, map, mergeMap, retry, share, shareReplay, startWith, switchMap } from 'rxjs/operators';
+import { catchError, map, mergeMap, retry, share, shareReplay, startWith, switchMap, tap } from 'rxjs/operators';
 import { BookStoreService } from '../shared/book-store.service';
 
 
@@ -14,18 +14,20 @@ import { BookStoreService } from '../shared/book-store.service';
 export class BookDetailsComponent {
 
   showDetails = false;
+  isLoading = false;
 
   book$ = this.router.paramMap.pipe(
     map(paramMap => paramMap.get('isbn')),
+    tap(() => this.isLoading = true),
     switchMap(isbn => this.bs.getSingle(isbn).pipe(
       retry(3),
       catchError((err: HttpErrorResponse) => of({
         title: 'FEHLER',
         description: err.message,
         rating: 0
-      })),
-      startWith(undefined)
-    ))
+      }))
+    )),
+    tap(() => this.isLoading = false),
   );
 
   constructor(private router: ActivatedRoute, private bs: BookStoreService) {
