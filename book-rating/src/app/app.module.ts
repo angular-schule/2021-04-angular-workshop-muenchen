a0d9267cd847670b136685228c4c9e60b4828490
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-04 15:37:12 +0200

    preparing @ngrx/router-store

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { DEFAULT_ROUTER_FEATURENAME, routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
+import { CustomRouterStateSerializer } from './utils-ngrx-router/serializer';
 // import { DashboardComponent } from './dashboard/dashboard.component';
 // import { BookComponent } from './books/book/book.component';
 
@@ -22,9 +24,22 @@ import { environment } from '../environments/environment';
     AppRoutingModule,
     // BooksModule,
     HttpClientModule,
-    StoreModule.forRoot({}, {}),
+
+
+
+    StoreModule.forRoot(
+      { [DEFAULT_ROUTER_FEATURENAME]: routerReducer },
+    ),
+    StoreRouterConnectingModule.forRoot({
+      serializer: CustomRouterStateSerializer,
+      stateKey: DEFAULT_ROUTER_FEATURENAME,
+    }),
+
+
+
     EffectsModule.forRoot([]),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
